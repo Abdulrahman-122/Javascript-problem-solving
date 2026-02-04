ce3b4bd1e40edd179e7d5d0ac95dca47222ab817
Abdulrahman Qasim <abdulrahman11510.qasim@gmail.com>
2026-02-04 21:50:03 +0200

    Add first problem solution

---
+++ JS_learning/first1.js
@@ -0,0 +1,65 @@
+// let x=5;
+// let y=10;
+// console.log(x+y);
+// console.log('Hello Js')
+
+
+var today=new Date();        // Date is the class contain ; year,month,day,hour,min,sec,milisecond
+
+var day=today.getDate();    //this return the number of the day (0(sunday);6(saturday))
+
+var daylist=['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
+console.log(`Today is ${daylist[day]}.`)
+
+var hour=today.getHours()
+var minute=today.getMinutes()
+var second=today.getSeconds()
+console.log(hour)
+console.log(minute)
+console.log(second)
+var prepand=(hour>=12)?"PM":"AM";
+console.log(prepand)
+hour=(hour>=12)?hour-12:hour;
+console.log(hour)  //render the hours from 0;12 instead of 0;24 
+if(hour==='12'&&prepand==='PM'){
+    if(minute===0&&second===0){
+        hour=12;
+        prepand='Noun';
+    }
+    else{
+        hour=12;
+        prepand='PM';
+    }
+}
+if(hour===12&&prepand==="AM"){
+    if(minute===0&&second===0){
+        hour=12;
+        prepand='midnight';
+    }
+    else{
+        hour=12;
+        prepand='AM';
+    }
+}
+console.log(`Today is ${daylist[day]}.`);
+console.log(`Current Time : ${hour} ${prepand} :${minute} :${second}.`);
+
+
+
+
+// Today is : Thursday.
+// Current Time : 7 PM : 25 : 10
+// 11:59:59 AM  → AM
+// 12:00:00 PM  → NOON  ← first condition
+// 12:00:01 PM  → PM    ← second condition
+// 12:30:00 PM  → PM
+// 12:59:59 PM  → PM
+
+
+
+
+
+
+
+
+
