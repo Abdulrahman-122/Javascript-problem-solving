d8e67cd7d443d418163d134dd3ce69591c67d0f1
Abdulrahman Qasim <abdulrahman11510.qasim@gmail.com>
2026-02-22 22:33:07 +0200

    Add 1st Dom problem to the box

---
+++ Dom_problems/problem1.js
@@ -0,0 +1,13 @@
+function js_style(){
+    let paragraph=document.getElementById('text');
+    paragraph.style.fontFamily='Arial, sans-serif';
+    paragraph.style.fontSize='30px';
+    paragraph.style.color='blue';
+    paragraph.style.background='yellow';
+
+}
+
+let header=document.getElementById('header')
+header.style.color='Green';
+header.style.background='pink';
+
